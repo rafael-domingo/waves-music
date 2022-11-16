0eb26b15c7ca439cab16fc6b3c7aa889b78cd6e0
Rafael Domingo <38447983+rafael-domingo@users.noreply.github.com>
2022-11-15 22:25:47 -0600

    implement audio playback

--- waves-music-player/src/App.js
+++ waves-music-player/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Import styles
 import './styles/app.scss';
 // Adding Components
@@ -8,10 +8,15 @@ import Song from './components/Song';
 import data from './util';
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
     </div>
   );
 }
